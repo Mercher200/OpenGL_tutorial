6ff7af17be0aaa9dd6cc1056a9ffe1fbca87d190
Dmitriy <60214129a@gmail.com>
2024-10-16 02:56:50 +0300

    Project_4 start

---
+++ Project_4/main.cpp
@@ -0,0 +1,52 @@
+#include "glew.h"
+#include "glfw3.h"
+
+#include <iostream>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if(glewInit() != GLEW_OK) std::cout << "ERROR !!!" << std::endl;
+
+    std::cout<<glGetString(GL_VERSION) <<std::endl;
+
+    float positions[6] = {
+        -0.5f , -0.5f ,
+         0.0f ,  0.0f ,
+         0.0f , -0.5f ,
+    }; 
+
+    uint32_t buffer;
+    glGenBuffers(1, &buffer); //создание буффера
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); //привязка буффера
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); // заполнение буффера
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3); // если нет индексного буффера
+        // glDrawElements(); //использую индексный буффер
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
