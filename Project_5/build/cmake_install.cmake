d9d8d7f2ba4c3ea2a0918932994b0faf2346104e
Dmitriy <60214129a@gmail.com>
2024-10-28 01:54:40 +0300

    creating projecy_5, drawing a rectangle

--- Project_5/build/cmake_install.cmake
+++ Project_5/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/GitHub/projects/OpenGL_tutorial/Project_4
+# Install script for directory: E:/GitHub/projects/OpenGL_tutorial/Project_5
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "E:/GitHub/projects/OpenGL_tutorial/Project_4/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "E:/GitHub/projects/OpenGL_tutorial/Project_5/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
